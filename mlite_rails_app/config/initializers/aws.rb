d3e012ee1229dc32ae65d312e930d2fc5c5a0315
Ziad Sabri <zsabri@emich.edu>
2024-11-03 13:47:06 -0500

    Fixed the file empty issue from github

--- mlite_rails_app/config/initializers/aws.rb
+++ mlite_rails_app/config/initializers/aws.rb
@@ -6,3 +6,16 @@ Aws.config.update({
                   })
 
 S3_BUCKET = Aws::S3::Resource.new.bucket('my-production-datasets')
+
+# config/initializers/aws.rb
+require 'aws-sdk-sqs'
+
+Aws.config.update({
+                    region: 'us-east-1',
+                    credentials: Aws::Credentials.new(
+                      ENV['AWS_ACCESS_KEY_ID'],
+                      ENV['AWS_SECRET_ACCESS_KEY']
+                    )
+                  })
+
+SQS_CLIENT = Aws::SQS::Client.new
